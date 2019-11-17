d1c2675cd82e94fb86edc5422e5fef57dd90a070
Zac <a19860101@gmail.com>
2019-11-17 12:29:43 +0800

    20191117

--- students/index.php
+++ students/index.php
@@ -22,18 +22,37 @@
 <body>
     <h1>學生資料</h1>
     <table>
-    
+        <tr>
+            <th>ID</th>
+            <th>姓名</th>
+            <th>E-Mail</th>
+            <th>教育程度</th>
+            <th>性別</th>
+            <th>技能</th>
+        </tr>
+        <?php while($row = mysqli_fetch_assoc($result)){ ?>
+        <tr>
+            <td><?php echo $row["id"];?></td>
+            <td><?php echo $row["name"];?></td>
+            <td><?php echo $row["email"];?></td>
+            <td><?php echo $row["edu"];?></td>
+            <td><?php echo $row["gender"];?></td>
+            <td><?php echo $row["skill"];?></td>
+        </tr>
+        <?php } ?>
+
+
     <?php
-        while($row = mysqli_fetch_assoc($result)){
-            echo "<tr>";
-            echo "<td>".$row["id"]."</td>";
-            echo "<td>".$row["name"]."</td>";
-            echo "<td>".$row["email"]."</td>";
-            echo "<td>".$row["edu"]."</td>";
-            echo "<td>".$row["gender"]."</td>";
-            echo "<td>".$row["skill"]."</td>";
-            echo "</tr>";        
-        }
+        // while($row = mysqli_fetch_assoc($result)){
+        //     echo "<tr>";
+        //     echo "<td>".$row["id"]."</td>";
+        //     echo "<td>".$row["name"]."</td>";
+        //     echo "<td>".$row["email"]."</td>";
+        //     echo "<td>".$row["edu"]."</td>";
+        //     echo "<td>".$row["gender"]."</td>";
+        //     echo "<td>".$row["skill"]."</td>";
+        //     echo "</tr>";        
+        // }
     ?>
     </table>
 
