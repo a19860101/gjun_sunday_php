6943b97a1e4b81711698389e54134f4b92907cd8
Zac <a19860101@gmail.com>
2019-11-17 12:06:26 +0800

    20191117

--- students/index.php
+++ students/index.php
@@ -10,24 +10,6 @@
     $sql = "SELECT * FROM students";
     $result = mysqli_query($conn,$sql);
     // var_dump($result);
-    $row = mysqli_fetch_assoc($result);
-    // var_dump($row);
-    echo $row["id"];
-    echo $row["name"];
-    echo $row["email"];
-    echo $row["gender"];
-    echo $row["edu"];
-    echo $row["skill"];
-
-    echo "<br>";
-    $row = mysqli_fetch_assoc($result);
-    // var_dump($row);
-    echo $row["id"];
-    echo $row["name"];
-    echo $row["email"];
-    echo $row["gender"];
-    echo $row["edu"];
-    echo $row["skill"];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -39,5 +21,16 @@
 </head>
 <body>
     <h1>學生資料</h1>
+    <?php
+        while($row = mysqli_fetch_assoc($result)){
+            echo $row["id"];
+            echo $row["name"];
+            echo $row["email"];
+            echo $row["edu"];
+            echo $row["gender"];
+            echo $row["skill"];
+            echo "<br>";
+        }
+    ?>
 </body>
 </html>
