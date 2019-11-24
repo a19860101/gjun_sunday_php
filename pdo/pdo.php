aa8b824a1fc8f1719a3e86d184a2491dc744ffed
Zac <a19860101@gmail.com>
2019-11-24 14:27:37 +0800

    20191124

--- pdo/pdo.php
+++ pdo/pdo.php
@@ -5,7 +5,15 @@
     $db_name = "sunday";
     $db_charset = "utf8mb4";
 
-    $dsn = "mysql:host{$db_host};dbname={$db_name};charset={$db_charset}";
+    // $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
     // $dsn = "mysql:host".$db_host.";dbname=".$db_name.";charset=".$db_charset;
-    $pdo = new PDO($dsn,$db_user,$db_pw);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    // $pdo = new PDO($dsn,$db_user,$db_pw);
+    // $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    try {
+        $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
+        $pdo = new PDO($dsn,$db_user,$db_pw);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
