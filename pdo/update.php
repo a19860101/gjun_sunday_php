7a1f6c718e1301030098212a8c2fa2986420968c
Zac <a19860101@gmail.com>
2019-11-24 16:06:54 +0800

    20191124

--- pdo/update.php
+++ pdo/update.php
@@ -1,19 +1,20 @@
 <?php
-    include("db.php");
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $edu = $_POST["edu"];
-    $gender = $_POST["gender"];
-    $skills = implode(",",$_POST["skills"]);
-    $id = $_POST["id"];
-
-    $sql = "UPDATE students SET 
-            name = '$name',
-            email = '$email',
-            edu = '$edu',
-            gender = '$gender',
-            skill = '$skills'
-            WHERE id =".$id;
-    
-    mysqli_query($conn,$sql);
-    header("location:index.php");
+	date_default_timezone_set("Asia/Taipei");
+	try {
+		include("pdo.php"); 
+		$name = $_POST["name"];
+		$email = $_POST["email"];
+		$edu = $_POST["edu"];
+		$gender = $_POST["gender"];
+		$skills = implode(",",$_POST["skills"]);
+		$updated_at = date("Y-m-d H:i:s");
+		$id = $_POST["id"];         
+		$sql = "UPDATE students SET name=?,email=?,edu=?,gender=?,skill=?,updated_at=? WHERE id=?";
+		
+		$stmt = $pdo->prepare($sql);
+		$stmt ->execute([$name,$email,$edu,$gender,$skills,$updated_at,$id]);
+		
+		header("location:index.php");
+	}catch(PDOException $e){
+		echo $e->getMessage();
+	}
