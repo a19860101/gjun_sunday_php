082157145e9491fc3ee8243e0cff61dceffa13b5
Zac <a19860101@gmail.com>
2019-11-24 14:58:23 +0800

    20191124

--- pdo/store.php
+++ pdo/store.php
@@ -1,12 +1,21 @@
 <?php
-    include("db.php");
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $edu = $_POST["edu"];
-    $gender = $_POST["gender"];
-    $skills = implode(",",$_POST["skills"]);
+    try{
+        include("pdo.php");
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $edu = $_POST["edu"];
+        $gender = $_POST["gender"];
+        $skills = implode(",",$_POST["skills"]);
+        $created_at = date("Y-m-d H:i:s");
+        $updated_at = date("Y-m-d H:i:s");;
 
-    $sql = "INSERT INTO students(name,email,edu,gender,skill)VALUES('$name','$email','$edu','$gender','$skills')";
-    mysqli_query($conn,$sql);
+        $sql = "INSERT INTO students(name,email,edu,gender,skill,created_at,updated_at)VALUES(?,?,?,?,?,?,?)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$name,$email,$edu,$gender,$skills,$created_at,$updated_at]);
+        
+        header("location:index.php");
 
-    header("location:index.php");
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+    
