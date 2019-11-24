0ccd6328cb46eb48dc5c91801c2afcea96e17a87
Zac <a19860101@gmail.com>
2019-11-24 11:35:03 +0800

    20191124

--- students_function/index.php
+++ students_function/index.php
@@ -36,6 +36,10 @@
                 <td><?php echo $row["edu"];?></td>
                 <td><?php echo $row["gender"];?></td>
                 <td><?php echo $row["skill"];?></td>
+                <td>
+                    <a href="delete.php?id=<?php echo $row["id"];?>" onclick="return confirm('確認刪除？')">刪除</a>
+                    <a href="edit.php?id=<?php echo $row["id"];?>">編輯</a>
+                </td>
             </tr>
         <?php } ?>
     </table>
