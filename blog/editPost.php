135108460c089ff5fecadeb9e5b407869d3ff96b
Zac <a19860101@gmail.com>
2019-12-08 10:58:43 +0800

    20191208

---
+++ blog/editPost.php
@@ -0,0 +1,39 @@
+<?php
+    include("function.php");
+    $row = showPost($_GET["id"]);
+?>
+<?php include("template/header.php"); ?>
+<?php include("template/nav.php"); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <h2>編輯文章</h2>
+        </div>
+        <div class="col-md-8">
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="title">文章標題</label>
+                    <input type="text" name="title" id="title" class="form-control" value="<?php echo $row["title"];?>">
+                </div>
+                <div class="form-group">
+                    <label for="content">文章內容</label>
+                    <textarea name="content" id="content" rows="10" class="form-control"><?php echo $row["content"];?></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="c_id">分類</label>
+                    <select name="c_id" id="c_id" class="form-control">
+                        <option value="1">體育</option>
+                        <option value="2">科技</option>
+                        <option value="3">生活</option>
+                    </select>
+                </div>
+                <input type="submit" class="btn btn-primary" value="儲存" name="submit">
+                <input type="button" class="btn btn-danger" value="取消" onclick="history.back()">
+            </form>
+
+        </div>
+    </div>
+</div>
+
+<?php include("template/footer.php"); ?>
--- blog/index.php
+++ blog/index.php
@@ -22,6 +22,7 @@
             </div>
             <a href="showPost.php?id=<?php echo $row["id"];?>" class="btn btn-primary">繼續閱讀</a>
             <a href="deletePost.php?id=<?php echo $row["id"]; ?>" class="btn btn-danger" onclick="return confirm('確認刪除？')">刪除</a>
+            <a href="editPost.php?id=<?php echo $row["id"]; ?>" class="btn btn-success">編輯</a>
             <div>建立時間:<?php echo $row["created_at"];?></div>
             <div>更新時間:<?php echo $row["updated_at"];?></div>
         </div>
