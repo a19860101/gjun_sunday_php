251b53362e863e66f3a3661a551696b74c981538
Zac <a19860101@gmail.com>
2019-12-22 14:05:07 +0800

    20191222

--- blog/backend/index.php
+++ blog/backend/index.php
@@ -6,14 +6,33 @@
 <?php include("template/nav.php"); ?>
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-8">
-        <h1>文章列表</h1>
+        <div class="col-12">
+            <h1>文章列表</h1>
         </div>
-        <?php foreach($rows as $row){ ?>
-        <div class="col-8">
-            <h2><?php echo $row["title"];?></h2>
+        <div class="col-12">
+            <table class='table'>
+                <tr>
+                    <th>#</th>
+                    <th>文章標題</th>
+                    <th>作者</th>
+                    <th>建立時間</th>
+                    <th>最後修改時間</th>
+                    <th></th>
+                </tr>
+                <?php foreach($rows as $row){ ?>
+                <tr>
+                    <td><?php echo $row["id"];?></td>
+                    <td><?php echo $row["title"];?></td>
+                    <td><?php echo $row["name"];?></td>
+                    <td><?php echo $row["created_at"];?></td>
+                    <td><?php echo $row["updated_at"];?></td>
+                    <td>
+                        <a href="#" class="btn btn-danger">刪除</a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </table>
         </div>
-        <?php }?>
 
     </div>
 </div>
